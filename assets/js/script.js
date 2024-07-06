75a94c26eec50918b94d7a6201cd8d2d5427636d
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-07-07 02:08:12 +0700

    menu toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
-// ======================== Slick Slider ========================
+// Check screen resize add menu toggle
+const menuResponsive = document.getElementById('menuRespon');
+const menuBurActive = document.getElementById('headtop-burger');
+function menuRespon() {
+  if (window.innerWidth < 720) {
+    menuResponsive.classList.add('tl-headtop-menu-respon');
+    menuBurActive.style.display = 'block';
+  } else {
+    menuResponsive.classList.remove('tl-headtop-menu-respon');
+    menuBurActive.style.display = 'none';
+  }
 
+  return(menuRespon, menuBurActive);
+}
+
+
+
+menuRespon(); 
+window.addEventListener('resize', menuRespon); // Us function screen resize always
+
+// Headtop menu Burger active toggle
+menuBurActive.addEventListener('click', function() {
+  menuBurActive.classList.toggle('tl-headtop-bur-active');
+  menuResponsive.classList.toggle('tl-headtop-toggle');
+});
+
+
+
+
+// ======================== Slick Slider ========================
 // ------------ Slider ------------
 $('.tl-slider').slick({
     dots: true,
